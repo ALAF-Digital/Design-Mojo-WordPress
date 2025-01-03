c8f4ceae63a37ad2cef2658aec0162874b9011c7
Musaib Ali <159254591+musaib-alaf@users.noreply.github.com>
2025-01-03 11:06:42 +0500

    our work changes

--- template-our-work.php
+++ template-our-work.php
@@ -103,82 +103,285 @@
             <div class="row align-items-center g-5">
                 <div class="col-md-6 order-md-2 order-lg-2">
                     <div class="our-workText">
-                        <h1>Digital Strategy Development</h1>
-                        <p>We create customized strategies that align with your business goals, using data-driven
-                            insights to enhance your brand's online presence.</p>
-                        <a href="<?php echo get_site_url(); ?>/Contact">Contact Us</a>
+
+                        <?php
+                        if (have_rows('website_development')):
+                            while (have_rows('website_development')):
+                                the_row();
+                                // Get sub field values.
+                                $heading = get_sub_field('heading');
+                                $description = get_sub_field('description');
+                                ?>
+                                <h1><?php echo $heading ?></h1>
+                                <p><?php echo $description ?></p>
+                                <a href="<?php echo get_site_url(); ?>/Contact">Contact Us</a>
+                            <?php endwhile;
+                        endif;
+                        wp_reset_query();
+                        ?>
                     </div>
                 </div>
 
                 <div class="col-md-6 order-md-1 order-lg-1">
-                    <div class="swiper myswiper9">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <div class="our-card">
-                                    <img src="<?php echo get_template_directory_uri() . '/images/our.png' ?>"
-                                        class="img-fluid" alt="">
+                    <?php
+                    if (have_rows('website_development')):
+                        while (have_rows('website_development')):
+                            the_row(); ?>
+                            <div class="swiper myswiper9">
+                                <div class="swiper-wrapper">
 
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="our-card">
-                                    <img src="<?php echo get_template_directory_uri() . '/images/our.png' ?>"
-                                        class="img-fluid" alt="">
+                                    <?php
+                                    // Get sub field values.
+                                    if (have_rows('projects')):
+                                        while (have_rows('projects')):
+                                            the_row();
+                                            ?>
+                                            <div class="swiper-slide">
+                                                <div class="our-card">
+                                                    <img src="<?php echo get_sub_field('images'); ?>" class="img-fluid" alt="">
 
+                                                </div>
+                                            </div>
+                                        <?php endwhile;
+                                    endif;
+                                    wp_reset_query();
+                                    ?>
                                 </div>
+                                <div class="swiper-pagination"></div>
                             </div>
-                            <div class="swiper-slide">
-                                <div class="our-card">
-                                    <img src="<?php echo get_template_directory_uri() . '/images/our.png' ?>"
-                                        class="img-fluid" alt="">
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-pagination"></div>
-                    </div>
+                        <?php endwhile;
+                    endif;
+                    wp_reset_query();
+                    ?>
                 </div>
             </div>
 
             <div class="row align-items-center g-5">
                 <div class="col-md-6  order-lg-1">
                     <div class="our-workText">
-                        <h1>Digital Strategy Development</h1>
-                        <p>We create customized strategies that align with your business goals, using data-driven
-                            insights to enhance your brand's online presence.</p>
-                        <a href="<?php echo get_site_url(); ?>/Contact">Contact Us</a>
+                        <?php
+                        if (have_rows('mobile_app_design')):
+                            while (have_rows('mobile_app_design')):
+                                the_row();
+                                // Get sub field values.
+                                $heading = get_sub_field('heading');
+                                $description = get_sub_field('description');
+                                ?>
+                                <h1><?php echo $heading ?></h1>
+                                <p><?php echo $description ?></p>
+                                <a href="<?php echo get_site_url(); ?>/Contact">Contact Us</a>
+                            <?php endwhile;
+                        endif;
+                        wp_reset_query();
+                        ?>
                     </div>
                 </div>
 
                 <div class="col-md-6  order-lg-2">
-                    <div class="swiper myswiper9">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <div class="our-card">
-                                    <img src="<?php echo get_template_directory_uri() . '/images/our.png' ?>"
-                                        class="img-fluid" alt="">
+                    <?php
+                    if (have_rows('mobile_app_design')):
+                        while (have_rows('mobile_app_design')):
+                            the_row();
 
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="our-card">
-                                    <img src="<?php echo get_template_directory_uri() . '/images/our.png' ?>"
-                                        class="img-fluid" alt="">
+                            ?>
+                            <div class="swiper myswiper9">
+                                <div class="swiper-wrapper">
+                                    <?php
+                                    // Get sub field values.
+                                    if (have_rows('projects')):
+                                        while (have_rows('projects')):
+                                            the_row();
+                                            ?>
+                                            <div class="swiper-slide">
+                                                <div class="our-card">
+                                                    <img src="<?php echo get_sub_field('images'); ?>"
+                                                        class="img-fluid" alt="">
 
+                                                </div>
+                                            </div>
+                                        <?php endwhile;
+                                    endif;
+                                    wp_reset_query();
+                                    ?>
                                 </div>
+                                <div class="swiper-pagination"></div>
                             </div>
-                            <div class="swiper-slide">
-                                <div class="our-card">
-                                    <img src="<?php echo get_template_directory_uri() . '/images/our.png' ?>"
-                                        class="img-fluid" alt="">
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-pagination"></div>
-                    </div>
+                        <?php endwhile;
+                    endif;
+                    wp_reset_query();
+                    ?>
                 </div>
             </div>
+
+            <div class="row align-items-center g-5">
+                <div class="col-md-6 order-md-2 order-lg-2">
+                    <div class="our-workText">
+
+                        <?php
+                        if (have_rows('web_app_design')):
+                            while (have_rows('web_app_design')):
+                                the_row();
+                                // Get sub field values.
+                                $heading = get_sub_field('heading');
+                                $description = get_sub_field('description');
+                                ?>
+                                <h1><?php echo $heading ?></h1>
+                                <p><?php echo $description ?></p>
+                                <a href="<?php echo get_site_url(); ?>/Contact">Contact Us</a>
+                            <?php endwhile;
+                        endif;
+                        wp_reset_query();
+                        ?>
+                    </div>
+                </div>
+
+                <div class="col-md-6 order-md-1 order-lg-1">
+                    <?php
+                    if (have_rows('web_app_design')):
+                        while (have_rows('web_app_design')):
+                            the_row(); ?>
+                            <div class="swiper myswiper9">
+                                <div class="swiper-wrapper">
+
+                                    <?php
+                                    // Get sub field values.
+                                    if (have_rows('projects')):
+                                        while (have_rows('projects')):
+                                            the_row();
+                                            ?>
+                                            <div class="swiper-slide">
+                                                <div class="our-card">
+                                                    <img src="<?php echo get_sub_field('images'); ?>" class="img-fluid" alt="">
+
+                                                </div>
+                                            </div>
+                                        <?php endwhile;
+                                    endif;
+                                    wp_reset_query();
+                                    ?>
+                                </div>
+                                <div class="swiper-pagination"></div>
+                            </div>
+                        <?php endwhile;
+                    endif;
+                    wp_reset_query();
+                    ?>
+                </div>
+            </div>
+
+            <div class="row align-items-center g-5">
+                <div class="col-md-6  order-lg-1">
+                    <div class="our-workText">
+                        <?php
+                        if (have_rows('social_media_marketing')):
+                            while (have_rows('social_media_marketing')):
+                                the_row();
+                                // Get sub field values.
+                                $heading = get_sub_field('heading');
+                                $description = get_sub_field('description');
+                                ?>
+                                <h1><?php echo $heading ?></h1>
+                                <p><?php echo $description ?></p>
+                                <a href="<?php echo get_site_url(); ?>/Contact">Contact Us</a>
+                            <?php endwhile;
+                        endif;
+                        wp_reset_query();
+                        ?>
+                    </div>
+                </div>
+
+                <div class="col-md-6  order-lg-2">
+                    <?php
+                    if (have_rows('social_media_marketing')):
+                        while (have_rows('social_media_marketing')):
+                            the_row();
+
+                            ?>
+                            <div class="swiper myswiper9">
+                                <div class="swiper-wrapper">
+                                    <?php
+                                    // Get sub field values.
+                                    if (have_rows('projects')):
+                                        while (have_rows('projects')):
+                                            the_row();
+                                            ?>
+                                            <div class="swiper-slide">
+                                                <div class="our-card">
+                                                    <img src="<?php echo get_sub_field('images'); ?>"
+                                                        class="img-fluid" alt="">
+
+                                                </div>
+                                            </div>
+                                        <?php endwhile;
+                                    endif;
+                                    wp_reset_query();
+                                    ?>
+                                </div>
+                                <div class="swiper-pagination"></div>
+                            </div>
+                        <?php endwhile;
+                    endif;
+                    wp_reset_query();
+                    ?>
+                </div>
+            </div>
+
+            <div class="row align-items-center g-5">
+                <div class="col-md-6 order-md-2 order-lg-2">
+                    <div class="our-workText">
+
+                        <?php
+                        if (have_rows('seo')):
+                            while (have_rows('seo')):
+                                the_row();
+                                // Get sub field values.
+                                $heading = get_sub_field('heading');
+                                $description = get_sub_field('description');
+                                ?>
+                                <h1><?php echo $heading ?></h1>
+                                <p><?php echo $description ?></p>
+                                <a href="<?php echo get_site_url(); ?>/Contact">Contact Us</a>
+                            <?php endwhile;
+                        endif;
+                        wp_reset_query();
+                        ?>
+                    </div>
+                </div>
+
+                <div class="col-md-6 order-md-1 order-lg-1">
+                    <?php
+                    if (have_rows('seo')):
+                        while (have_rows('seo')):
+                            the_row(); ?>
+                            <div class="swiper myswiper9">
+                                <div class="swiper-wrapper">
+
+                                    <?php
+                                    // Get sub field values.
+                                    if (have_rows('projects')):
+                                        while (have_rows('projects')):
+                                            the_row();
+                                            ?>
+                                            <div class="swiper-slide">
+                                                <div class="our-card">
+                                                    <img src="<?php echo get_sub_field('images'); ?>" class="img-fluid" alt="">
+
+                                                </div>
+                                            </div>
+                                        <?php endwhile;
+                                    endif;
+                                    wp_reset_query();
+                                    ?>
+                                </div>
+                                <div class="swiper-pagination"></div>
+                            </div>
+                        <?php endwhile;
+                    endif;
+                    wp_reset_query();
+                    ?>
+                </div>
+            </div>
+            
         </div>
     </div>
 </section>
